dd9b20e0ea77b527a76f81d88dff1686cc3e6538
Rishi Soni <rishi.soni@colorado.edu>
2017-05-04 09:33:38 +0000

    Final commit

--- simple-hough-eliptical-interactive/Profiling/capture_individual.cpp
+++ simple-hough-eliptical-interactive/Profiling/capture_individual.cpp
@@ -35,7 +35,6 @@ struct timespec thread_dt = {0,0};
 #define NSEC_PER_MICROSEC (1000)
 long array1[1000];
 double res[100];
-cpu_set_t affinity;
 
 /**********************************Thread Globals**************************************/
 using namespace cv;
@@ -45,7 +44,7 @@ pthread_attr_t CV_attr, main_attr, motor_attr,ultrasonic_attr;
 
 pthread_mutex_t vector_mutex;
 pthread_mutex_t ultrasonic_mutex;
-sem_t sem_image, sem_ultra;
+
 struct sched_param CV_param;
 struct sched_param main_param;
 struct sched_param motor_param;
@@ -112,10 +111,7 @@ int main( int argc, char** argv )
     }
 	motor_initialize();
 	driver_initialize();
-	
-	CPU_ZERO(&affinity);
-	CPU_SET(0, &affinity);
-	
+
 	pthread_mutex_init(&vector_mutex, NULL);
 	pthread_mutex_init(&ultrasonic_mutex, NULL);
 
@@ -126,31 +122,24 @@ int main( int argc, char** argv )
 	
 	pthread_attr_setinheritsched(&CV_attr, PTHREAD_EXPLICIT_SCHED);
 	pthread_attr_setschedpolicy(&CV_attr, SCHED_FIFO);
-	pthread_attr_setaffinity_np(&CV_attr, sizeof(cpu_set_t), &affinity);
-
+	
 	pthread_attr_setinheritsched(&motor_attr, PTHREAD_EXPLICIT_SCHED);
 	pthread_attr_setschedpolicy(&motor_attr, SCHED_FIFO);
-	pthread_attr_setaffinity_np(&motor_attr, sizeof(cpu_set_t), &affinity);
 
 	pthread_attr_setinheritsched(&ultrasonic_attr, PTHREAD_EXPLICIT_SCHED);
 	pthread_attr_setschedpolicy(&ultrasonic_attr, SCHED_FIFO);
-	pthread_attr_setaffinity_np(&ultrasonic_attr, sizeof(cpu_set_t), &affinity);
 	
 	pthread_attr_setinheritsched(&main_attr, PTHREAD_EXPLICIT_SCHED);
 	pthread_attr_setschedpolicy(&main_attr, SCHED_FIFO);
-	pthread_attr_setaffinity_np(&main_attr, sizeof(cpu_set_t), &affinity);	
-
+	
 	max_prio = sched_get_priority_max(SCHED_FIFO);
 	min_prio = sched_get_priority_min(SCHED_FIFO);
 	
 	main_param.sched_priority = max_prio;
-	CV_param.sched_priority = max_prio - 3;
-	motor_param.sched_priority = max_prio - 1;
+	CV_param.sched_priority = max_prio - 1;
+	motor_param.sched_priority = max_prio - 3;
 	ultrasonic_param.sched_priority = max_prio - 2;
 	
-	sem_init(&sem_image, 0, 1);
-	sem_init(&sem_ultra, 0, 0);
-
 	int rc = sched_setscheduler(getpid(), SCHED_FIFO, &main_param);
 
 	if(rc)
@@ -166,30 +155,29 @@ int main( int argc, char** argv )
 	pthread_attr_setschedparam(&main_attr, &main_param);
 
 
-	if(pthread_create(&CV_thread, &CV_attr, CVThread, (void *)0))
-	{
-		printf("Error creating CV thread\n");
-		perror(NULL);		
-		exit(1);
-	}
+	//if(pthread_create(&CV_thread, &CV_attr, CVThread, (void *)0))
+	//{
+	//	printf("Error creating thread\n");
+	//	exit(1);
+	//}
+	
 	
 	if(pthread_create(&motor_thread, &motor_attr, MotorThread, (void *)0))
 	{
 		printf("Error creating  motor thread\n");
-		perror(NULL);
 		exit(1);
 	}
 	
+	/*
 	if(pthread_create(&ultrasonic_thread, &ultrasonic_attr, UltrasonicThread, (void *)0))
 	{
-		printf("Error creating  ultra thread\n");
-		perror(NULL);
+		printf("Error creating  motor thread\n");
 		exit(1);
 	}
-	
-	pthread_join(ultrasonic_thread, NULL);
-	pthread_join(CV_thread, NULL);
+	*/
+	//pthread_join(CV_thread, NULL);
 	pthread_join(motor_thread, NULL);
+	//pthread_join(ultrasonic_thread, NULL);
 }
 
 
@@ -201,44 +189,42 @@ void *CVThread(void *)
     cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, HRES);
     cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, VRES);
     
-    while(1)
+    for(int k=0; k<1000;k++)
     {
-	//sem_wait(&sem_image);
-	//printf("Thread image\n");
+	clock_gettime(CLOCK_REALTIME, &start);
     	frame=cvQueryFrame(capture);
+
         Mat mat_frame(frame);
         cvtColor(mat_frame, gray, CV_BGR2GRAY);
         GaussianBlur(gray, gray, Size(9,9), 2, 2);
 		
 	pthread_mutex_lock(&vector_mutex);
-        HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 1, gray.rows/8, 100, 50, 5, 40);
-	pthread_mutex_unlock(&vector_mutex);	
-
+        HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 1, gray.rows/8, 100, 50, 30, 40);
+//	pthread_mutex_unlock(&vector_mutex);	
+//	printf("before while loop\n");
         for( size_t i = 0; i < circles.size(); i++ )
         {
-		pthread_mutex_lock(&vector_mutex);
-		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
+//			pthread_mutex_lock(&vector_mutex);
+			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+			int radius = cvRound(circles[i][2]);
 
-		//printf("X axis is = %f\n", circles[i][0]);
-		//printf("Y axis is = %f\n", circles[i][1]);
-		//printf("Radius is = %f\n", circles[i][2]);
+			//printf("X axis is = %f\n", circles[i][0]);
+			//printf("Y axis is = %f\n", circles[i][1]);
+			//printf("Radius is = %f\n", circles[i][2]);
 
-		//if(circles[i][0] >= (float)280 && circles[i][0] <= (float)360)
-		//{
-		//printf("OKAY\n");
-		//}
-		//else
-		//{
-		//printf("NOT OKAY\n");
-		//}
-
-		//pthread_mutex_unlock(&vector_mutex);
-
-		// circle center
-		circle( mat_frame, center, 3, Scalar(0,255,0), -1, 8, 0 );
-		// circle outline
-		circle( mat_frame, center, radius, Scalar(0,0,255), 3, 8, 0 );
+			//if(circles[i][0] >= (float)300 && circles[i][0] <= (float)380)
+			//{
+			//printf("OKAY\n");
+			//}
+			//else
+			//{
+			//printf("NOT OKAY\n");
+			//}
+//			pthread_mutex_unlock(&vector_mutex);
+			// circle center
+			circle( mat_frame, center, 3, Scalar(0,255,0), -1, 8, 0 );
+			// circle outline
+			circle( mat_frame, center, radius, Scalar(0,0,255), 3, 8, 0 );
         }
 	pthread_mutex_unlock(&vector_mutex);
      
@@ -249,42 +235,37 @@ void *CVThread(void *)
         char c = cvWaitKey(10);
         if( c == 'q' )	
 		break;
-	//usleep(30000);
-	//sem_post(&sem_ultra);
+	clock_gettime(CLOCK_REALTIME, &end);
+	delta_t(&end, &start, &thread_dt);
+	printf("Thread Image completed at time stamp: %lf msec\n",(double)(thread_dt.tv_nsec / 1000000));
+	//printf("Test Conducted over %d and value is %f \n", k, array1[k]);
+	//usleep(100);
    }
+    calc_avg(array1);
     cvReleaseCapture(&capture);
     cvDestroyWindow("Capture Example");	
 }
 
 
 
-
-/* GPIOPIN 50 & 58 = Left wheel
-   GPIOPIN 55 and 52 = Right wheel 
-*/
-
+/*
 void *MotorThread(void *)
 {	
-	while(1)
+	for(int k=0; k<1000;k++)
 	{
-		//printf("motor thread\n");
+		clock_gettime(CLOCK_REALTIME, &start);
 		pthread_mutex_lock(&vector_mutex);
-		pthread_mutex_lock(&ultrasonic_mutex);
-		//for(size_t i = 0; i < circles.size(); i++)
-		if(circles.size() > 0)		
+		for( size_t i = 0; i < circles.size(); i++)
 		{	
-			if (circles[0][0] >= 480)
-			{	
-				//Left turn 
+			if (circles[i][0] >= float(340) && circles[i][0] <= float(380))
+			{
 				gpio_set_value(GPIOPIN50, HIGH);
 				gpio_set_value(GPIOPIN58, LOW);
 				gpio_set_value(GPIOPIN55, LOW);
 				gpio_set_value(GPIOPIN52, LOW);
-
 			}
-			else if (circles[0][0] <= 160)
-			{	
-				//Right turn 
+			else if (circles[i][0] >= float(140) && circles[i][0] <= float(180))
+			{
 				gpio_set_value(GPIOPIN50, LOW);
 				gpio_set_value(GPIOPIN58, LOW);
 				gpio_set_value(GPIOPIN55, HIGH);
@@ -292,24 +273,22 @@ void *MotorThread(void *)
 			}
 			else
 			{
-				if(c>80)
-				{
-					gpio_set_value(GPIOPIN50, HIGH);
-					gpio_set_value(GPIOPIN58, LOW);
-					gpio_set_value(GPIOPIN55, HIGH);
-					gpio_set_value(GPIOPIN52, LOW);
-				}
-				else
-				{
-					gpio_set_value(GPIOPIN50, LOW);
-					gpio_set_value(GPIOPIN58, LOW);
-					gpio_set_value(GPIOPIN55, LOW);
-					gpio_set_value(GPIOPIN52, LOW);
-				}
-			}
-
+				gpio_set_value(GPIOPIN50, LOW);
+				gpio_set_value(GPIOPIN58, LOW);
+				gpio_set_value(GPIOPIN55, LOW);
+				gpio_set_value(GPIOPIN52, LOW);
+			}			
 		}
-
+		pthread_mutex_unlock(&vector_mutex);
+		
+		pthread_mutex_lock(&ultrasonic_mutex);
+		if(c>50)
+		{
+			gpio_set_value(GPIOPIN50, HIGH);
+			gpio_set_value(GPIOPIN58, LOW);
+			gpio_set_value(GPIOPIN55, LOW);
+			gpio_set_value(GPIOPIN52, HIGH);
+		}
 		else
 		{
 			gpio_set_value(GPIOPIN50, LOW);
@@ -317,46 +296,103 @@ void *MotorThread(void *)
 			gpio_set_value(GPIOPIN55, LOW);
 			gpio_set_value(GPIOPIN52, LOW);
 		}
-		/*else
-		{
-			if(c>110)
+		pthread_mutex_unlock(&ultrasonic_mutex);
+		clock_gettime(CLOCK_REALTIME, &end);
+		delta_t(&end, &start, &thread_dt);
+		array1[cnt]=(double)(thread_dt.tv_nsec / 1000000);
+	}
+	calc_avg(array1);
+        //usleep(10);
+}
+*/
+
+void *MotorThread(void *)
+{	
+	int a=1;
+	int b=1;
+	long int i=0;
+	for(int k=0; k<1000;k++)
+	{
+		clock_gettime(CLOCK_REALTIME, &start);
+		pthread_mutex_lock(&vector_mutex);
+			if (a==1)
 			{
 				gpio_set_value(GPIOPIN50, HIGH);
 				gpio_set_value(GPIOPIN58, LOW);
 				gpio_set_value(GPIOPIN55, LOW);
-				gpio_set_value(GPIOPIN52, HIGH);
+				gpio_set_value(GPIOPIN52, LOW);
+				a--;
 			}
-			else
+			else if (a==0)
+			{
+				gpio_set_value(GPIOPIN50, LOW);
+				gpio_set_value(GPIOPIN58, LOW);
+				gpio_set_value(GPIOPIN55, HIGH);
+				gpio_set_value(GPIOPIN52, LOW);
+				a=a+2;
+			}
+			else if (a==2)
 			{
 				gpio_set_value(GPIOPIN50, LOW);
 				gpio_set_value(GPIOPIN58, LOW);
 				gpio_set_value(GPIOPIN55, LOW);
 				gpio_set_value(GPIOPIN52, LOW);
-			}
-		}*/
+				a--;
+			}			
 		pthread_mutex_unlock(&vector_mutex);
+		
+		pthread_mutex_lock(&ultrasonic_mutex);
+		if(c==1)
+		{
+			gpio_set_value(GPIOPIN50, HIGH);
+			gpio_set_value(GPIOPIN58, LOW);
+			gpio_set_value(GPIOPIN55, LOW);
+			gpio_set_value(GPIOPIN52, HIGH);
+			c--;
+		}
+		else if(c==0)
+		{
+			gpio_set_value(GPIOPIN50, LOW);
+			gpio_set_value(GPIOPIN58, LOW);
+			gpio_set_value(GPIOPIN55, LOW);
+			gpio_set_value(GPIOPIN52, LOW);
+			c++;
+		}
 		pthread_mutex_unlock(&ultrasonic_mutex);
-		usleep(40000);
+		clock_gettime(CLOCK_REALTIME, &end);
+		delta_t(&end, &start, &thread_dt);
+		array1[k]=(thread_dt.tv_nsec);
+		printf("%d\n",k);
 	}
-        
+	calc_avg(array1);
+        //usleep(10);
 }
-
 
 void *UltrasonicThread(void *)
 {
-	while(1)
+	//for(int k=0; k<1000;k++)
+	int cnt = 0;	
+	while(1)	
 	{
-		//sem_wait(&sem_ultra);
-		//printf("Thread ultra\n");
+		clock_gettime(CLOCK_REALTIME, &start);
 		pthread_mutex_lock(&ultrasonic_mutex);
 		while(read(fd, &c, 1) <= 0){}
+		cnt++;		
 		printf("%d\n", c);
 		fflush(stdout);
 		pthread_mutex_unlock(&ultrasonic_mutex);
-		//sem_post(&sem_image);
-		usleep(25000);
+	
+		clock_gettime(CLOCK_REALTIME, &end);
+		delta_t(&end, &start, &thread_dt);
+		array1[cnt]=(double)(thread_dt.tv_nsec / 1000000);
+		if(cnt >= 1000)
+			break;		
+		//usleep(5);	
 	}
+	calc_avg(array1);
 }
+
+
 
 
 void driver_initialize(void)
@@ -370,6 +406,8 @@ void driver_initialize(void)
 	configure = (struct termios*)malloc(sizeof(struct termios));
 	tty_config(configure, fd);
 }
+
+
 
 
 void tty_config(struct termios *con, int descriptor)
@@ -403,4 +441,55 @@ void motor_initialize(void)
 	gpio_set_dir(GPIOPIN58, OUTPUT_PIN); 			// The LED is an output
 	gpio_set_dir(GPIOPIN55, OUTPUT_PIN);
 	gpio_set_dir(GPIOPIN52, OUTPUT_PIN);
+}
+
+int delta_t(struct timespec *stop, struct timespec *start, struct timespec *delta_t)
+{
+  int dt_sec=stop->tv_sec - start->tv_sec;
+  int dt_nsec=stop->tv_nsec - start->tv_nsec;
+
+  if(dt_sec >= 0)
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+    }
+  }
+  else
+  {
+    if(dt_nsec >= 0)
+    {
+      delta_t->tv_sec=dt_sec;
+      delta_t->tv_nsec=dt_nsec;
+    }
+    else
+    {
+      delta_t->tv_sec=dt_sec-1;
+      delta_t->tv_nsec=NSEC_PER_SEC+dt_nsec;
+    }
+  }
+
+  return(1);
+}
+
+
+void calc_avg(long *arr)
+{
+	for(int j=1; j < 100; j++)
+	{
+		double sum = 0;
+		for(int x=1; x < 10; x++)
+		{
+			sum +=(double)*(arr + j*10 + x);
+		}
+		*(res + j) = sum/10;
+
+		printf("Avg at index %d is %fnsec\n", j, *(res + j));
+	}
 }
